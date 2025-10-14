7181bc1af352fffb3ed8d2d4eb520808c7bab64b
James <james@ammortal.com>
2025-10-14 11:58:12 -0400

    add grid class

--- grid.py
+++ grid.py
@@ -1,3 +1,14 @@
-def print_grid(grid):
-    for row in grid:
-        print(" ".join(f"{cell:2}" for cell in row))
+from dataclasses import dataclass
+
+@dataclass
+class Grid:
+    rows: int
+    cols: int
+    w: float
+    h: float
+
+def print_grid(grid: Grid):
+    for row in range(grid.rows):
+        for col in range(grid.cols):
+            print(f"({col * grid.w}, {row * grid.h})", end=" ")
+        print()
