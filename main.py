1c62c0b6c0a2661d39cc9d96ed47e51c4dcee0af
James <james@ammortal.com>
2025-10-14 11:36:09 -0400

    add parser

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pdb import main
+import argparse
 
 def main():
-    print("AMRAS Triangle Mapper")
+    print("AMRAS Triangle Mapper Console App")
+    ap = argparse.ArgumentParser(description="AMRAS Triangle Mapper Console App")
 
 if __name__ == '__main__':
     main()
